79c364ed1ad7e8a4ed0e19215017cb52ac6d08ca
JessicaVillalobos <jessica.villalobos9@gmail.com>
2026-06-13 22:03:37 +0200

    Add comments for different nominal + backup options

--- legged_controllers/MPS_tests/main_mps.py
+++ legged_controllers/MPS_tests/main_mps.py
@@ -2,7 +2,11 @@ from simulation import TestManager
 import rospy
 
 if __name__ == '__main__':
-    tm = TestManager(use_nn = True, only_mpc = True, only_rl = False, nom_rl = False)
+    # No MPS: use_nn = False, only_mpc = True, only_rl = False, nom_rl = False
+    # MPS = MPC+MPC: use_nn = True, only_mpc = True, only_rl = False, nom_rl = False
+    # MPS = MPC+RL: use_nn = True, only_mpc = False, only_rl = False, nom_rl = False
+    # MPS = RL+RL: use_nn = True, only_mpc = False, only_rl = True, nom_rl = False
+    tm = TestManager(use_nn = True, only_mpc = False, only_rl = True, nom_rl = False)
     try:
         tm.run_simulations()
 
